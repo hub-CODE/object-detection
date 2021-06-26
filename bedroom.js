394bcbc6e28f94ce39b39086497c3de449251f81
hub-CODE <66951180+hub-CODE@users.noreply.github.com>
2021-06-26 11:44:04 +0300

    Add files via upload

--- bedroom.js
+++ bedroom.js
@@ -4,3 +4,22 @@ img = "";
 function preload(){
    img = LoadImage('bedroom.jpg');
 }
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    status = true;
+    console.log("Model Loaded!");
+}
+
+function gotResults(error, results){
+if (error) 
+{
+    console.log(error);
+}
+}
